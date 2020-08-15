3d6d908b686c5a56664facff5fedcdb46b6e4a8e
Ibrahim Tarhini <tarhini.forever@gmail.com>
2020-08-15 16:11:22 +0300

    make it better

--- server.js
+++ server.js
@@ -74,9 +74,9 @@ app.use('/api/post', post);
 //app.use('/static', express.static(path.join(__dirname, 'client/build')));
 app.use(express.static(__dirname + '/client/build'));
 
-/*app.get('*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});*/
+});
 
 // Port
 const PORT = process.env.PORT || 5000;
